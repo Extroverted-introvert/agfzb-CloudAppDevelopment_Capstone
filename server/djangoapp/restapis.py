b54e5bdead537c3dbf3f18ccd30928894f98c1a0
Parth Tripathi <parth.tripathi17@gmail.com>
2022-04-29 20:07:34 +0000

    [Updation]: Added get review serverless

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from .models import CarDealer
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
 
@@ -36,7 +36,6 @@ def get_dealers_from_cf(url, **kwargs):
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        print(json_result)
         dealers = json_result['body']['data']["rows"]
         # For each dealer object
         for dealer in dealers:
@@ -51,17 +50,12 @@ def get_dealers_from_cf(url, **kwargs):
 
     return results
 
-# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a DealerView object list
 def get_dealers_from_cf_state(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
     json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
-        print(json_result)
         dealers = json_result['body']['data']["docs"]
         # For each dealer object
         for dealer in dealers:
@@ -72,6 +66,31 @@ def get_dealers_from_cf_state(url, **kwargs):
                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                    short_name=dealer_doc["short_name"],
                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
+# def get_dealer_by_id_from_cf(url, dealerId):
+# - Call get_request() with specified arguments
+# - Parse JSON results into a DealerView object list
+def get_dealer_reviews_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    print(url)
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        print(json_result)
+        dealers = json_result['body']['data']["docs"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer
+            dealer_obj = DealerReview(dealership=dealer_doc["dealership"], name=dealer_doc["name"], purchase=dealer_doc["purchase"], review=dealer_doc["review"],
+                                   purchase_date=dealer_doc["purchase_date"], car_make=dealer_doc["car_make"], car_model=dealer_doc["car_model"],
+                                   car_year=dealer_doc["car_year"],
+                                   sentiment=dealer_doc["sentiment"], id=dealer_doc["id"])
             results.append(dealer_obj)
 
     return results
